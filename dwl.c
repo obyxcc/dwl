f69e79316c26262822ec9d4ac73d6fc40ffb4a0f
obyx <cole@obyx.cc>
2024-05-23 08:05:23 -0400

    added alwayscenter patch

--- dwl.c
+++ dwl.c
@@ -519,6 +519,10 @@ applyrules(Client *c)
 			}
 		}
 	}
+	if (mon) {
+		c->geom.x = (mon->w.width - c->geom.width) / 2 + mon->m.x;
+		c->geom.y = (mon->w.height - c->geom.height) / 2 + mon->m.y;
+	}
 	setmon(c, mon, newtags);
 }
 
